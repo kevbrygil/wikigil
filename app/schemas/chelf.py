7943e2b44871453af6b7def1d5c20701cb4a28a6
Gilbert Ignacio Tamayo Rios <tamayo_144@hotmail.com>
2021-03-23 06:20:51 -0600

    wiki

---
+++ app/schemas/chelf.py
@@ -0,0 +1,22 @@
+from marshmallow_jsonapi import Schema, fields
+from marshmallow import validate
+
+class StoreSchema(Schema):
+ 
+    not_blank = validate.Length(min=1, error='Los campos no deben estar en blanco')
+
+    id = fields.Integer(dump_only=True)   
+    name = fields.String(validate=not_blank)
+    address = fields.String(validate=not_blank)
+    latitud = fields.String()
+    longitud = fields.String()
+
+    # self links
+    def get_top_level_links(self, data, many):
+        if many:
+            self_link = "/stores"
+        else:
+            self_link = "/stores/{}".format(data['id'])
+        return {'self': self_link}
+    class Meta:
+        type_ = 'store'
